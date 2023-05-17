4880bf5abf13a0e78b7844c58bcc3d3380be8ca9
abohoss <21p0141@eng.asu.edu.eg>
2023-05-17 16:59:50 +0300

    lots of commits

--- FishFoodStore/store/models.py
+++ FishFoodStore/store/models.py
@@ -1,6 +1,5 @@
 from django.db import models
-from django.urls import reverse
-from django.core.validators import MinLengthValidator
+from django.core import validators
 
 # Create your models here.
 class Customer(models.Model):
@@ -8,7 +7,7 @@ class Customer(models.Model):
     password = models.CharField(max_length=100)
     firstName = models.CharField(max_length=20)
     lastName = models.CharField(max_length=20)
-    phone = models.CharField(max_length=11, validators=[MinLengthValidator(11)])
+    phone = models.PositiveIntegerField()
     district = models.CharField(max_length=50)
     streetName = models.CharField(max_length=50)
     buildingNumber = models.PositiveIntegerField()
@@ -17,16 +16,10 @@ class Customer(models.Model):
     delivered = models.BooleanField(default=False)
     cart = models.ManyToManyField('Product', blank=True)
 
-    
-    def __str__(self) -> str:
-        return f"{self.firstName} {self.lastName}"
-
 class Product(models.Model):
     name = models.CharField(max_length=64)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     quantity = models.IntegerField()
-    customer = models.ManyToManyField(Customer, blank=True, related_name="customers")
-
 class Order(models.Model):
     STATUS_CHOICES = [
         ('Pending', 'Pending'),
